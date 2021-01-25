17e646d5c36ad38732e52ede97b7c5f776386f65
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-25 10:19:48 -0500

    adding es6 template literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Luke";
+const lName = "MacLean";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fName} ${lName}`}!</h1>
+    <p>You are {Math.floor(Math.random() * 32)} years old</p>
+  </div>,
+  document.getElementById("root")
+);
